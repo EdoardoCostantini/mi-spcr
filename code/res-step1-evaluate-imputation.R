51ebe67ef95758e6511ad5e95fa253ed867ffdc1
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2022-07-13 17:36:22 +0200

    feat: add script to evaluate imputations

---
+++ code/res-step1-evaluate-imputation.R
@@ -0,0 +1,47 @@
+# Project:   mi-spcr
+# Objective: Script to evaluate imputations
+# Author:    Edoardo Costantini
+# Created:   2022-07-13
+# Modified:  2022-07-13
+
+# Prep environment -------------------------------------------------------------
+
+  rm(list = ls()) # to clean up
+  source("./init.R") # only for support functions
+
+# Load Results -----------------------------------------------------------------
+
+  loaction <- "../output/"
+  run_name <- "20220713-163954-trial-pc-unzipped" # toy run on pc
+  out <- readRDS(paste0(loaction, run_name, ".rds"))
+
+
+# absence of non-convergence ---------------------------------------------------
+
+  # Convergence plots for all conditions
+  lapply(1:length(out$mids), function (i){
+    plot(out$mids[[i]], main = names(out$mids)[i])
+  })
+
+# posterior predictive checks / fit of the imputation model --------------------
+
+# distributional characteristics of the imputations ----------------------------
+
+# imputation distribution vs observed data distribution ------------------------
+
+  # Box and wisker plots
+  lapply(1:length(out$mids), function (i){
+    bwplot(
+      out$mids[[i]],
+      X1 + X2 + X3 ~ .imp,
+      main = names(out$mids)[i]
+    )
+  })
+
+  # Densities
+  lapply(1:length(out$mids), function (i){
+    densityplot(
+      out$mids[[i]],
+      main = names(out$mids)[i]
+    )
+  })
