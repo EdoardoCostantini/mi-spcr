285f64104dde9da03f8e2dc3351502326d19d868
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2022-08-01 19:52:37 +0200

    fix: misc fixes for lisa cluster

--- code/sim-lisa-step1-storeInfo.R
+++ code/sim-lisa-step1-storeInfo.R
@@ -2,16 +2,25 @@
 # Objective: Store the session information
 # Author:    Edoardo Costantini
 # Created:   2022-07-29
-# Modified:  2022-07-29
+# Modified:  2022-08-01
 
-## Prepare Environment
+# Prepare Environment
 rm(list = ls())
 
-## Initialize the environment:
-source("./init.R")
+# Initialize the environment:
+source("./init-objects.R")
+source("./init-software.R")
 
-## Prepare storing results
-source("./fs.R")
+# Subset conditions?
+subset_cond <- TRUE
+if(subset_cond == TRUE){
+  cnds <- cnds %>%
+    filter(pm %in% c(.25),
+           nla %in% c(10),
+           npcs %in% c(1, 2),
+           mech %in% "MAR",
+           method %in% c("pcr", "fo"))
+}
 
 # Output Directory from Terminal inputs
 args <- commandArgs(trailingOnly = TRUE)
--- code/sim-lisa-step2-run-doRep.R
+++ code/sim-lisa-step2-run-doRep.R
@@ -2,27 +2,36 @@
 # Objective: Run a single repetition in lisa
 # Author:    Edoardo Costantini
 # Created:   2022-07-29
-# Modified:  2022-07-29
+# Modified:  2022-08-01
 
-## Make sure we have a clean environment:
-rm(list = ls(all = TRUE))
+# Prepare Environment
+rm(list = ls())
 
-## Initialize the environment:
-source("./init.R")
+# Initialize the environment:
+source("./init-objects.R")
+source("./init-software.R")
 
-## Prepare storing results
-source("./fs.R")
+# Subset conditions?
+subset_cond <- TRUE
+if(subset_cond == TRUE){
+  cnds <- cnds %>%
+    filter(pm %in% c(.25),
+           nla %in% c(10),
+           npcs %in% c(1, 2),
+           mech %in% "MAR",
+           method %in% c("pcr", "fo"))
+}
 
-## Extract commandline arguments
+# Extract commandline arguments
 args      <- commandArgs(trailingOnly = TRUE)
 rp        <- as.numeric(args[1]) # replication rp = 1 to desired
 fs$out_dir <- args[2]   # overwrite output directory defined
 
-## Example Inputs Not to run
+# Example Inputs Not to run
 # rp        <- 1
 # fs$out_dir <- "../output/trash/"
 
-## Run one replication of the simulation:
+# Run one replication of the simulation:
 runRep(rp = rp,
        cnds = cnds,
        parms = parms,
