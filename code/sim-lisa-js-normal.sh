87d557db25e56710dbd01e911e40602f761a49d1
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2022-08-04 11:00:29 +0200

    feat: bring js normal up to date with js short

--- code/sim-lisa-js-normal.sh
+++ code/sim-lisa-js-normal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH -p normal
-#SBATCH -t 10:00:00
+#SBATCH -t 46:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=e.costantini@tilburguniversity.edu
 
@@ -27,33 +27,27 @@
 module load R
 
 ## Define Variables and Directories
-projDir=$HOME/mi-pcr	  # Project directory
+projDir=$HOME/mi-pcr	        # Project directory
 inDir=$projDir/code           # Source directory (for R)
 ncores=`sara-get-num-cores` 	# Number of available cores
 idJob=$SLURM_ARRAY_JOB_ID  	  # Master ID for the array of jobs
 idTask=$SLURM_ARRAY_TASK_ID 	# Array index for the current job
 
-## Define Output Directories
-# Temporary
-tmpOut="$TMPDIR"/$idJob\_$idTask
+# Temporary output
+tmpOut="$TMPDIR"/$idJob-$idTask
 mkdir -p $tmpOut
 
-# Final
+# Final output
 outDir=$projDir/output/$idJob
 	if [ ! -d "$outDir" ]; then 	# create if missing
 	    mkdir -p $outDir
 	fi
 
-## Allow worker nodes to find my personal R packages:
-export R_LIBS=$HOME/R/x86_64-pc-linux-gnu-library/4.1/
-# for R_LIBS explain: https://statistics.berkeley.edu/computing/R-packages
-# this is probably overkill but keep it in the loop for safety (and maybe ask Kyle about it)
-
 ## Store the stopos pool's name in the environment variable STOPOS_POOL:
 export STOPOS_POOL=pool
 
 ## Loop Over Cores
-for (( i=1; i<ncores ; i++ )) ; do
+for (( i=1; i<=ncores ; i++ )) ; do
 (
 	## Get the next line or parameters from the stopos pool:
 	stopos next
@@ -63,16 +57,16 @@ for (( i=1; i<ncores ; i++ )) ; do
 	    break
 	fi
 
-	## If it's the first Stopos value, then Run the Rscript to store session info
+  # If it's the first Stopos value, then store session info
 	if [ $STOPOS_VALUE = 1 ]; then
-	    Rscript ./sim-lisa-step1-storeInfo.R $outDir/
+	    Rscript $inDir/sim-lisa-step1-storeInfo.R $outDir/
 	fi
 
-	## Call the R script with the replication number from the stopos pool:
-	Rscript $inDir/sim-lisa-step2-run-doRep.R $STOPOS_VALUE $tmpOut/
+	# Call the R script with the replication number from the stopos pool:
+	Rscript $inDir/sim-lisa-step2-run-doRep.R $tmpOut/ $STOPOS_VALUE
 	# script_name.R --options repetition_counter output_directory
 
- 	## Remove the used parameter line from the stopos pool:
+ 	# Remove the used parameter line from the stopos pool:
 	stopos remove
 ) &
 done
@@ -84,7 +78,7 @@ wait
  cd $tmpOut/
 
  # Zip everything that is inside (./.)
- tar -czf "$TMPDIR"/$idJob\_$idTask.tar.gz ./.
+ tar -czf $tmpOut.tar.gz ./.
 
 ## Copy output from scratch to output directory:
- cp -a "$TMPDIR"/$idJob\_$idTask.tar.gz $outDir/
+ cp -a $tmpOut.tar.gz $outDir/
